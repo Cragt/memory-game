7c6fd9150b68da665e08e63f1387837e55acaf06
Cragt <cragt369@gmail.com>
2024-08-30 02:47:25 -0500

    Render Welcome component instead of App

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import Welcome from "./Welcome.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Welcome />
   </StrictMode>
 );
